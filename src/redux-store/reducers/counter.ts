b0ab7223c07448cbc4b8dba89a51d11553552c22
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 09:50:11 +0530

    Update counter.ts

--- src/redux-store/reducers/counter.ts
+++ src/redux-store/reducers/counter.ts
@@ -1,7 +1,10 @@
 /* node modules */
 import { createSlice } from "@reduxjs/toolkit";
 
-/* reducer */
+/**
+ * the reducer that takes care of the value of the counter. this will be controlled based on the
+ * action that we are sending into the reducer
+ **/
 const initialState = 0;
 const counterSlice = createSlice({
   name: "counter",
