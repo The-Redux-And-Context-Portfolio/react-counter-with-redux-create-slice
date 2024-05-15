7ac1d1e7f6a341f1c452940c4077f232943c5294
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 09:56:19 +0530

    Update use-on-reset.ts

--- src/components/hooks/use-on-reset.ts
+++ src/components/hooks/use-on-reset.ts
@@ -4,11 +4,16 @@ import { reset } from "../../redux-store/reducers/counter";
 import getSound from "../../redux-store/selectors/get-sound";
 import playResetSound from "../utils/reset-sound";
 
-/* hook */
+/**
+  * @returns - an event handler that helps set updates to the sound slice in the redux store
+  */
 function useOnReset() {
+  /* subscribe to the redux store */
   const sound = useAppSelector(getSound);
+  /* create a dispatch function that helps to send instructions to the store */
   const reduxDispatch = useAppDispatch();
 
+  /* event handler */
   function handleOnReset() {
     reduxDispatch(reset());
     sound && playResetSound();
